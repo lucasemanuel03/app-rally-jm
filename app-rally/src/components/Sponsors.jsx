6810c4eb65c882dd56d11f2270fc86ea42dd948c
Lucas Brollo <leblucas3@gmail.com>
2025-05-03 15:09:14 -0300

    agregando componente Sponsor, agregando recursos para sponsors

---
+++ app-rally/src/components/Sponsors.jsx
@@ -0,0 +1,39 @@
+function Sponsors() {
+  return (
+    <div className="flex flex-col justify-center items-center px-3">
+        <h2 className="text-2xl text-amber-400 font-bold">SPONSORS OFICIALES</h2>
+        <div className="flex flex-row items-center py-5">
+            <div className="grid grid-cols-2 grid-rows-3 sm:grid-cols-3 gap-2 p-2">
+                <a href="https://www.coloniacaroya.gov.ar" target="_blank" rel="noopener noreferrer">
+                    <img className="w-70 " src="/images/sponsors/caroya.png" alt="Logo Colonia Caroya"></img>
+                </a>
+                <a href="https://www.jesusmaria.gov.ar" target="_blank" rel="noopener noreferrer">
+                    <img className="w-70 " src="/images/sponsors/JM-logo.png" alt="Logo Jesús María"></img>
+                </a>
+                <a href="https://www.sinsacate.gov.ar" target="_blank" rel="noopener noreferrer">
+                    <img className="w-70 " src="/images/sponsors/Sinsacate.png" alt="Logo Sinsacate"></img>
+                </a>
+                <a href="https://www.cba.gov.ar" target="_blank" rel="noopener noreferrer">
+                    <img className="w-70 " src="/images/sponsors/cba-2.001.png" alt="Logo Córdoba"></img>
+                </a>
+                <a href="https://www.cba.gov.ar" target="_blank" rel="noopener noreferrer">
+                    <img className="w-70 " src="/images/sponsors/Cba-3.001.png" alt="Logo Córdoba 3"></img>
+                </a>
+                <a href="https://www.coca-cola.com.ar" target="_blank" rel="noopener noreferrer">
+                    <img className="w-70 " src="/images/sponsors/Coca.png" alt="Logo Coca-Cola"></img>
+                </a>
+                <a href="https://www.lavoz.com.ar" target="_blank" rel="noopener noreferrer">
+                    <img className="w-70 " src="/images/sponsors/La-Voz.png" alt="Logo La Voz"></img>
+                </a>
+                <a href="https://www.paseshow.com.ar" target="_blank" rel="noopener noreferrer">
+                    <img className="w-70 " src="/images/sponsors/Paseshow.png" alt="Logo Pase Show"></img>
+                </a>
+                <a href="https://www.ypf.com" target="_blank" rel="noopener noreferrer">
+                    <img className="w-70 " src="/images/sponsors/YPF-2.001.png" alt="Logo YPF"></img>
+                </a>
+            </div>
+        </div>
+    </div>
+  );
+}
+export default Sponsors;
